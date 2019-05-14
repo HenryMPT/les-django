25403280072abc95dabd28a675f557f13eeb08aa
JOSÉ TEIXEIRA <a52107@ualg.pt>
2019-05-14 20:07:44 +0100

    henry's disappearing template

--- Users/views.py
+++ Users/views.py
@@ -86,6 +86,10 @@ class OrganizationDelete(DeleteView):
 	model = Organization
 	sucess_url = "&empresas"
 	template_name = "processes/forms/organization_confirm_delete.html"
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['org_users'] = User.objects.filter(organization__id =self.kwargs['pk'])
+		return context	
 
 class OrganizationUpdate(UpdateView):
 	model = Organization
